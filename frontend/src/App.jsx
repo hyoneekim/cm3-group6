f6787df19e45dee7975c7cd4aa1d56b0e7bca5bc
Noah Stewart <natstrawets@gmail.com>
2025-10-03 17:59:47 +0300

    -fixed auth bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import AddJobPage from "./pages/AddJobPage";
 import Navbar from "./components/Navbar";
-import NotFoundPage from "./pages/NotFoundPage"
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage from "./pages/JobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add-job" element={<AddJobPage />} />
+            <Route path="/edit-job/:id" element={<EditJobPage />} />
+            <Route path="/job-page/:id" element={<JobPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/add-job" element={<AddJobPage />} />
-              <Route path='*' element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  
-  export default App;
+export default App;
