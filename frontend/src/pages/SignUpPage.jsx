16e04c2105ca99c09a54375b399f787b0b40c9a3
Shreeya <shreeyap@metropolia.fi>
2025-10-03 17:18:08 +0300

    Signup

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,62 @@
+import useField from "../hooks/useField";
+import useSignup from "../hooks/useSignup";
+import { useNavigate } from "react-router-dom";
+
+const Signup = ({ setIsAuthenticated }) => {
+  const navigate = useNavigate();
+  const name = useField("text");  
+  const username = useField("text");
+  const password = useField("password");
+  const phoneNumber = useField("text");
+  const gender = useField("text");
+  const dateOfBirth = useField("date");
+  const membershipStatus = useField("text");
+  const address = useField("text");
+
+  const { handleSignup, error } = useSignup(setIsAuthenticated);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const success = await handleSignup({
+      username: username.value,
+      password: password.value,
+      name: name.value,
+      phone_number: phoneNumber.value,
+      gender: gender.value,
+      date_of_birth: dateOfBirth.value,
+      membership_status: membershipStatus.value,
+      address: address.value,
+    });
+    if (success) {
+      console.log("success");
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="create">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label>Name:</label>
+        <input {...name} />
+        <label>Username:</label>
+        <input {...username} />
+        <label>Password:</label>
+        <input {...password} />
+        <label>Phone Number:</label>
+        <input {...phoneNumber} />
+        <label>Gender:</label>
+        <input {...gender} />
+        <label>Date of Birth:</label>
+        <input {...dateOfBirth} />
+        <label>Membership Status:</label>
+        <input {...membershipStatus} />
+        <label>Address:</label>
+        <input {...address} />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
